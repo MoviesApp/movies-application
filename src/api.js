907b8000b5a7ca76deded0a2b0edb6081de79ad3
Carlos Teller <carlos.z.teller@gmail.com>
2020-03-13 15:02:14 -0500

    Added basic functionality of adding/editing/deleting movies

--- src/api.js
+++ src/api.js
@@ -1,19 +1,32 @@
 module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
-  },
+    getMovies: () => {
+        return fetch('/api/movies')
+            .then(response => response.json());
+    },
 
-  createMovies: (title, rating) => {
-    return fetch('/api/movies', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json',},
-          body: JSON.stringify({
-            title: title,
-            rating: rating
-          })
-        }
-    )
+    postMovie: (movie) => {
+        return fetch('/api/movies', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(movie)
+        })
         .then(response => response.json());
-  }
+    },
+
+    editMovie: (id, movie) => {
+        return fetch(`/api/movies/${id}`, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(movie)
+        })
+            .then(response => response.json());
+    },
+
+    deleteMovie: (id) => {
+        return fetch(`/api/movies/${id}`, {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'}
+        })
+            .then(response => response.json());
+    }
 };
--- src/index.js
+++ src/index.js
@@ -7,34 +7,70 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies, createMovies} = require('./api.js');
+const {getMovies, postMovie, editMovie, deleteMovie} = require('./api.js');
 const $ = require('jquery');
 
-$('.container').html('Loading...');
-
-let buildMoviesList = (id, title, rating) => {
-  $('.container').html('');
-  $('.container').append(`id#${id} - ${title} - rating: ${rating}`);
-}
+const renderMovies = (movies) => {
+  movies.forEach(({title, rating, id}) => {
+    console.log(`id#${id} - ${title} - rating: ${rating}`);
+  });
+};
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+  $('.loading').removeClass('active');
+  $('.movies').addClass('active');
 
-    buildMoviesList(`${id}`, `${title}`, `${rating}`);
-  });
-
-  $('.container').append('<button class="add-movie">Add Movie</button>').click(function() {
-    createMovies('Twilight', '0').then(() => {
-      console.log('Movie added...');
-      getMovies();
-    }).catch((error) => {
-      alert('Oh no! Something went wrong.\nCheck the console for details.')
-      console.log(error);
-    });
-  });
+  renderMovies(movies);
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+});
+
+$('.post-movie').click(function() {
+  $('.movies').removeClass('active');
+  $('.loading').addClass('active');
+
+  postMovie({ title: 'Lego Movie', rating: '3' }).then(data => getMovies().then(movies => {
+    $('.loading').removeClass('active');
+    $('.movies').addClass('active');
+
+    console.log('Here are all the movies:');
+    renderMovies(movies);
+  })).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+  });
+});
+
+$('.edit-movie').click(function() {
+  $('.movies').removeClass('active');
+  $('.loading').addClass('active');
+
+  editMovie(4, { title: 'The Lego Movie', rating: '4' }).then(data => getMovies().then(movies => {
+    $('.loading').removeClass('active');
+    $('.movies').addClass('active');
+
+    console.log('Here are all the movies:');
+    renderMovies(movies);
+  })).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+  });
+});
+
+$('.delete-movie').click(function() {
+  $('.movies').removeClass('active');
+  $('.loading').addClass('active');
+
+  deleteMovie(3).then(data => getMovies().then(movies => {
+    $('.loading').removeClass('active');
+    $('.movies').addClass('active');
+
+    console.log('Here are all the movies:');
+    renderMovies(movies);
+  })).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+  });
 });
