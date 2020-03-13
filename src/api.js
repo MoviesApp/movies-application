e3ead93b442c52725c401ef5b90d6b2ac7cb54cd
Carlos Teller <carlos.z.teller@gmail.com>
2020-03-13 13:43:20 -0500

    Changes testing functionality for creating movies.

--- src/api.js
+++ src/api.js
@@ -2,5 +2,18 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+
+  createMovies: (title, rating) => {
+    return fetch('/api/movies', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json',},
+          body: JSON.stringify({
+            title: title,
+            rating: rating
+          })
+        }
+    )
+        .then(response => response.json());
   }
 };
--- src/index.js
+++ src/index.js
@@ -7,12 +7,32 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies, createMovies} = require('./api.js');
+const $ = require('jquery');
+
+$('.container').html('Loading...');
+
+let buildMoviesList = (id, title, rating) => {
+  $('.container').html('');
+  $('.container').append(`id#${id} - ${title} - rating: ${rating}`);
+}
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+    buildMoviesList(`${id}`, `${title}`, `${rating}`);
+  });
+
+  $('.container').append('<button class="add-movie">Add Movie</button>').click(function() {
+    createMovies('Twilight', '0').then(() => {
+      console.log('Movie added...');
+      getMovies();
+    }).catch((error) => {
+      alert('Oh no! Something went wrong.\nCheck the console for details.')
+      console.log(error);
+    });
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
