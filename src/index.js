6fce83c8741d0669b7b1ac54c853b67821dd06d5
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-16 12:29:07 -0500

    more changes

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies, getMovie, editMovie, deleteMovie} = require('./api.js');
-
-// FULL STAR: <i class="fas fa-star"></i>
-// EMPTY STAR: <i class="far fa-star"></i>
+const {getMovies, getMovie, postMovie, editMovie, deleteMovie} = require('./api.js');
 
 const starfunction = (x) => {
   let stars;
@@ -39,8 +36,27 @@ const starfunction = (x) => {
 
 const renderMovies = (movies) => {
   $(".moviesdiv").html("");
-  movies.forEach(({title, rating, id}) => {
-    $(".moviesdiv").append(`<div class = "col-md-12">${title} ${starfunction(rating)}<button class="edit-movie-modal-btn btn btn-info" edit-id="${id}" data-target="#edit-movie-modal"><i class="fas fa-pencil-alt"></i></button><button class="delete-movie btn btn-danger" delete-id="${id}"><i class="fas fa-trash-alt"></i></button></div>`);
+  movies.forEach(({image, title, rating, id}) => {
+    if (image === "") {
+      image = "images/coming-soon.jpeg"
+    }
+    $(".moviesdiv").append(`
+<div class = "individualMovies card">
+<div class="card-body">
+
+<h5 class="card-title">${title}</h5>
+     <h6 class="card-subtitle mb-2 text-muted">${starfunction(rating)}</h6>
+     <img src="${image}" class="card-img-top" alt="image">
+ 
+<button class="edit-movie-modal-btn btn btn-info" edit-id="${id}" data-target="#edit-movie-modal">
+<i class="fas fa-pencil-alt"></i>
+</button>
+<button class="delete-movie btn btn-danger" delete-id="${id}">
+<i class="fas fa-trash-alt"></i>
+</button>
+
+</div>
+</div>`);
   });
 };
 
@@ -54,28 +70,44 @@ getMovies().then((movies) => {
   console.log(error);
 });
 
-$('.post-movie').click(function() {
+
+
+// Stuff for adding movies
+$(document).on('click', '.post-movie-modal-btn', function() {
+    $('#post-movie-modal').modal('show');
+
+    $('.modal-footer').html(`<button type="button" class="post-movie btn btn-primary">Submit</button>`);
+  });
+
+$(document).on('click', '.post-movie', function() {
   $('.movies').removeClass('active');
   $('.loading').addClass('active');
 
-  postMovie({ title: 'Lego Movie', rating: '3' }).then(data => getMovies().then(movies => {
+  postMovie({ title: $("#post-title-name").val(), rating: $("#post-rating-text").val(), image: $("#post-image-url").val()})
+      .then(data => getMovies()
+      .then(movies => {
     $('.loading').removeClass('active');
     $('.movies').addClass('active');
-
     console.log('Here are all the movies:');
     renderMovies(movies);
-  })).catch((error) => {
+  }))
+      .catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
   });
+  $('#post-movie-modal').modal('hide');
 });
 
+
+
+//Stuff for editing movies
 $(document).on('click', '.edit-movie-modal-btn', function() {
   $('#edit-movie-modal').modal('show');
 
   getMovie($(this).attr('edit-id')).then((data) => {
     $('#title-name').val(data.title);
     $('#rating-text').val(data.rating);
+    $('#edit-image-url').val(data.image);
   });
 
   $('.modal-footer').html(`<button type="button" class="edit-movie btn btn-primary" edit-id="${$(this).attr('edit-id')}">Submit</button>`);
@@ -85,7 +117,7 @@ $(document).on('click', '.edit-movie', function() {
   $('.movies').removeClass('active');
   $('.loading').addClass('active');
 
-  editMovie($(this).attr('edit-id'), { title: $('#title-name').val(), rating: $('#rating-text').val() }).then(data => getMovies().then(movies => {
+  editMovie($(this).attr('edit-id'), { title: $('#title-name').val(), rating: $('#rating-text').val(), image: $("#edit-image-url").val()}).then(data => getMovies().then(movies => {
     $('.loading').removeClass('active');
     $('.movies').addClass('active');
 
@@ -99,6 +131,9 @@ $(document).on('click', '.edit-movie', function() {
   $('#edit-movie-modal').modal('hide');
 });
 
+
+
+//Stuff for delete movies
 $(document).on('click', '.delete-movie', function() {
   console.log($(this).attr('delete'));
 
